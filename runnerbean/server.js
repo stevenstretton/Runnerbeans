6d78c7b483b79712e6089d2c9de2e3d9c0c68ca8
Steven Stretton <stevenstretton@stevens-mbp.home>
2017-01-15 01:21:29 +0000

    created new runnerbean module

---
+++ runnerbean/server.js
@@ -0,0 +1,8 @@
+var http = require('http');
+
+function onRequest(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write('Hello World');
+    res.end();
+}
+http.createServer(onRequest).listen(8000);
