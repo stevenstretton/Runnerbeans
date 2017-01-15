01b57063a0b493af0341732f6d2c7e03b3272a87
Steven Stretton <stretton.steven@gmail.com>
2017-01-15 20:00:14 +0000

    create to mongo implamented

--- runnerbean/app.js
+++ runnerbean/app.js
@@ -5,6 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var hbs = require('express-handlebars');
+var expressValidator = require('express-validator');
+var expressSession = require('express-session');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -21,8 +23,10 @@ app.set('view engine', 'hbs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(expressValidator());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(expressSession({secret: 'steve', saveUninitialized: false, resave: false}));
 
 app.use('/', index);
 app.use('/users', users);
